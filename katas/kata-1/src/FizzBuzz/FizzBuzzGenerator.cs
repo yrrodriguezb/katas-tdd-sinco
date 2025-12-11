e0037c57dba4a7293806ba7a17a838558df6ba69
Yeison Rodriguez <=>
2025-12-11 18:49:51 -0500

    🔵 FizzBuzz - Si_Ingreso5_Debe_RetornarLaListaCorrespondiente

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -4,14 +4,34 @@ namespace FizzBuzz
     {
         public static List<string> Generate(int count)
         {
-            return count switch
+            var lista =  new List<string>();
+
+            for (var numero = 1; numero <= count; numero++)
             {
-                1 => ["1"],
-                2 => ["1", "2"],
-                3 => ["1", "2", "Fizz"],
-                4 => ["1", "2", "Fizz", "4"],
-                5 => ["1", "2", "Fizz", "4", "Buzz"]
-            };
+                if (EsDivisibleTres(numero))
+                    lista.Add("Fizz");
+                else if (EsDivisiblePorCinco(numero))
+                    lista.Add("Buzz");
+                else
+                    lista.Add($"{numero}");
+            }
+            
+            return lista;
+        }
+
+        private static bool EsDivisiblePorCinco(int numero)
+        {
+            return EsDivisiblePor(numero,5);
+        }
+
+        private static bool EsDivisiblePor(int numero, int divisor)
+        {
+            return numero % divisor == 0;
+        }
+
+        private static bool EsDivisibleTres(int numero)
+        {
+            return EsDivisiblePor(numero, 3);
         }
     }
 }
