89fbe17b26ce0b53621c8db7c92834b105770c49
Yeison Rodriguez <yeison.rodriguez@sinco.com>
2025-12-18 15:19:23 -0500

    🔴 Calculator - Si_Sumo1y2_Debe_Retornar3

--- katas/kata-2/tests/Calculator.Tests/CalculatorTests.cs
+++ katas/kata-2/tests/Calculator.Tests/CalculatorTests.cs
@@ -21,6 +21,18 @@ namespace Calculator.Tests
             result.Should().Be(2);
         }
 
-       
+        [Fact] 
+        public void Si_Sumo1y2_Debe_Retornar3()
+        {
+            // Arrange
+            int a = 1;
+            int b = 2;
+
+            // Act
+            int result = _calculator.Add(a, b);
+
+            // Assert
+            result.Should().Be(3);
+        }
     }
 }
