5e0e0426376315692228840082a67e36b71f5f49
Yeison Rodriguez <yeison.rodriguez@sinco.com>
2025-12-18 18:28:11 -0500

    🔴 Año bisiesto - Si_IngresoElAnio2001_Debe_RetornarFalso

--- katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
+++ katas/kata-3/tests/LeapYear.Tests/LeapYearCheckerTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Xunit;
 
 namespace LeapYear.Tests
@@ -5,17 +6,10 @@ namespace LeapYear.Tests
     public class LeapYearCheckerTests
     {
         [Fact]
-        public void Year_DivisibleBy4_ButNotBy100_IsLeapYear()
-        {
-            // Ejemplo: 2004 es año bisiesto
-            Assert.True(LeapYearChecker.IsLeapYear(2004));
-        }
-
-        [Fact]
-        public void Year_NotDivisibleBy4_IsNotLeapYear()
+        public void Si_IngresoElAnio2001_Debe_RetornarFalso()
         {
             // Ejemplo: 2001 no es año bisiesto
-            Assert.False(LeapYearChecker.IsLeapYear(2001));
+            LeapYearChecker.IsLeapYear(2001).Should().BeFalse();
         }
     }
 }
