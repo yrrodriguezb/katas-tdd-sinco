619c82234e31bbcb601bec8fe315c92e52599633
Yeison Rodriguez <yeison.rodriguez@sinco.com>
2025-12-18 18:46:16 -0500

    🔵 Año bisiesto - Si_IngresoElAnio2008_Debe_RetornarVerdadero

--- katas/kata-3/src/LeapYear/LeapYearChecker.cs
+++ katas/kata-3/src/LeapYear/LeapYearChecker.cs
@@ -4,10 +4,7 @@ namespace LeapYear
     {
         public static bool IsLeapYear(int year)
         {
-            if (year is 2004 or 2008)
-                return true;
-            
-            return false;
+           return year % 4 == 0;
         }
     }
 }
