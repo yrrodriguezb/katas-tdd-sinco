1129b06ec87e0d014e42a69c06281d09478af0cb
Yeison Rodriguez <yeison.rodriguez@sinco.com>
2025-12-18 19:23:19 -0500

    🔵 Año bisisesto - Si_IngresoElAnio2800_Debe_RetornarTrue

--- katas/kata-3/src/LeapYear/LeapYearChecker.cs
+++ katas/kata-3/src/LeapYear/LeapYearChecker.cs
@@ -4,28 +4,20 @@ namespace LeapYear
     {
         public static bool IsLeapYear(int year)
         {
-            if (year == 2000 || year == 2400 || year == 2800)
+            if (EsDivisiblePor400(year))
                 return true;
-            
-            if (EsDivisiblePor100(year))
+            else if (EsDivisiblePor100(year))
                 return false;
-            
-            return EsDiviblePorCuatro(year);
+            else
+                return EsDivisiblePorCuatro(year);
         }
 
-        private static bool EsDivisiblePor100(int year)
-        {
-            return EsDivisiblePor(year, 100);
-        }
+        private static bool EsDivisiblePor400(int year) => EsDivisiblePor(year, 400);
 
-        private static bool EsDiviblePorCuatro(int year)
-        {
-            return EsDivisiblePor(year, 4);
-        }
+        private static bool EsDivisiblePor100(int year) => EsDivisiblePor(year, 100);
 
-        private static bool EsDivisiblePor(int year, int divisor)
-        {
-            return year % divisor == 0;
-        }
+        private static bool EsDivisiblePorCuatro(int year) => EsDivisiblePor(year, 4);
+
+        private static bool EsDivisiblePor(int year, int divisor) => year % divisor == 0;
     }
 }
