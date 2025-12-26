e476312b6823315092a53ee273c8f3ea2d6553c4
Yeison Rodriguez <yeison.rodriguez@sinco.com>
2025-12-26 13:09:31 -0500

    🟢 String Calculator - Si_IngresoTresNumerosSeparadoPorSaltoDeLineaYComa_Debe_Sumarlos

--- katas/kata-4/src/StringCalculator/StringCalculator.cs
+++ katas/kata-4/src/StringCalculator/StringCalculator.cs
@@ -4,11 +4,18 @@ namespace StringCalculator
     {
         public static int Calculate(string input)
         {
-            if (string.IsNullOrEmpty(input))
+            var str = NormalizarEntrada(input);
+            if (string.IsNullOrEmpty(str))
                 return 0;
 
-            var numbers = input.Split(',');
+            var numbers = str.Split(',');
+            
             return numbers.Sum(n => int.Parse(n));
+        }
+
+        private static string NormalizarEntrada(string input)
+        {
+            return input.Replace("\n", ",");
         }
     }
 }
